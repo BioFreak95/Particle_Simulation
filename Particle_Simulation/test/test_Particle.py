4a33916600cc7934f754f88b41dec2458d009206
Maximilian Salomon <32635839+biofreak95@users.noreply.github.com>
2018-02-01 16:58:59 +0100

    Bugfix NL

--- Particle_Simulation/test/test_Particle.py
+++ Particle_Simulation/test/test_Particle.py
@@ -7,5 +7,13 @@ class test_Particle(unittest.TestCase):
     def test_wrong_typeIndex(self):
         self.assertRaises(TypeError, Particle, 2.3, np.array([1, 1, 1]))
 
+    def test_trueParticle(self):
+        particle = Particle(2, np.array([1, 1]))
+        if isinstance(particle, Particle):
+            assert_ = True
+        else:
+            assert_ = False
+        self.assertTrue(assert_)
+
     def test_wrong_position(self):
         self.assertRaises(TypeError, Particle, 1, 23)
