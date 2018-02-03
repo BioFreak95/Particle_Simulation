c218a376ed0f7e7cbb50a595a5caeb13fe34c3ad
Mnlohani <manish.lohani@fu-berlin.de>
2018-02-03 16:36:04 +0100

    toolbox changes

--- Particle_Simulation/Toolbox.py
+++ Particle_Simulation/Toolbox.py
@@ -1,57 +1,91 @@
-import csv
+import os
 import numpy as np
-from numpy import float64
+from numpy import float64, dtype
 from Particle_Simulation.Particle import Particle
 from Particle_Simulation.ParticleType import ParticleType
 from Particle_Simulation.Parameters import Parameters
 
+# To_do to handle charge.temperature,update_radius,cut_off radius
+# taken charge as fourth input for parameters
 class Toolbox:
     
-    #Read data from Input_Particle.csv
-    def get_particle(particle_file_path):
-        input_particle = np.genfromtxt(particle_file_path, delimiter=",", skip_header=1, autostrip=True)
-        type_index = (input_particle[: , 0]).astype(int)
-        position = input_particle[ : , 1 : ] 
-        #Particle Object Creation
+    def get_inputs(parameters, types, positions, box):
+        #Saving input arrays into a single file in uncompressed .npz format
+        np.savez('Inputs.npz', parameters=parameters, types=types, positions=positions, box=box)
+        #Loading arrays from .npz format
+        with np.load('Inputs.npz') as fh :
+            parameters_fh = fh['parameters'] 
+            types_fh = fh['types']
+            positions_fh = fh['positions']
+            box_fh = fh['box']
+            return parameters_fh, types_fh, positions_fh, box_fh
+    
+    #Particle class variables creation:
+    def get_particle(parameters, types, positions, box):
+        #Load input arrays from .npz format
+        parameters_fh, types_fh, positions_fh, box_fh = Toolbox.get_inputs(parameters, types, positions, box)
+        #particle_name to integer value mapping for type_index 
+        #dictionary creation like {'A': 0, 'B': 1}
+        particle_names = list(parameters.keys()) # key part of parameters
+        dict_name_to_type= {}
+        for i, type_name  in enumerate(particle_names):
+            dict_name_to_type[type_name] = i
+        #type_index Integer array creation
+        type_index = [dict_name_to_type[name_key] for name_key in types_fh]
+        type_index = np.array(type_index)
+        position = positions_fh
+        #particle object creation
         particle_list = []
-        for i in range(len(input_particle)):
+        for i in range(len(type_index)):
             particle_obj = Particle(type_index=type_index[i], position=position[i])
             particle_list.append(particle_obj)
         particle = np.array(particle_list)
-        return particle
-    
-    #Read data from Input_PaticleType.csv
-    def get_particle_type(particle_typ_file_path):
-        in_particle_type = np.genfromtxt(particle_typ_file_path, delimiter=",", skip_header=1, autostrip=True, dtype='U')
-        name = in_particle_type[ : , 0]
-        mass = (in_particle_type[ : , 1]).astype(np.float64) 
-        charge = (in_particle_type[ : ,2]).astype(np.float64)
-        lj_epsilon = (in_particle_type[ : , 3]).astype(np.float64)
-        lj_sigma = (in_particle_type[ : , 4]).astype(np.float64)
+        os.remove('Inputs.npz')
+        return type_index, position, particle
+        
+    #ParticleType class variables creation
+    def get_particle_type(parameters, types, positions, box):
+        parameters_fh, types_fh, positions_fh, box_fh = Toolbox.get_inputs(parameters, types, positions, box)
+        # key part of parameters dictionary
+        particle_names = list(parameters.keys()) 
+        #value part of parameters.npy file made
+        x = parameters_fh.item() 
+        values = [x[name_key] for name_key in particle_names] 
+        values = np.array(values)
+        #ParticleType object creation
+        name = np.array(particle_names)
+        lj_sigma = values[ : , 0]
+        lj_epsilon = values[ : , 1]
+        mass = values[ : , 2]
+        charge = values[ : , 3]
         particle_type_list = []
-        for i in range(len(in_particle_type)):
+        for i in range(len(name)):
             particle_type_obj = ParticleType(name=name[i], mass=mass[i], charge=charge[i],
                                              lj_epsilon=lj_epsilon[i], lj_sigma=lj_sigma[i])
             particle_type_list.append(particle_type_obj)
         particle_type = np.array(particle_type_list)
-        return particle_type
+        os.remove('Inputs.npz')
+        return particle_type, name, mass, charge, lj_epsilon, lj_sigma  
     
-    #Read data from Input_Parameter.csv
-    def get_parameters(parameters_file_path, particle_typ_file_path):
-        in_parameters = np.genfromtxt(parameters_file_path, delimiter=",", skip_header=1, autostrip=True)
-        temperature = in_parameters[ : , 0]
-        box = in_parameters[ : , 1 : 4 ]
-        es_sigma = in_parameters[ : , 4]
-        update_radius = in_parameters[ : , 5]
-        cutoff_radius = in_parameters[ : , 6]
-        parameters_list = []
-        particle_types=Toolbox.get_particle_type(particle_typ_file_path)
-        for i in range(len(in_parameters)):
-            parameters__obj = Parameters(temperature=temperature[i], box=box[i], es_sigma=es_sigma[i], 
-                                         update_radius=update_radius[i], particle_types=particle_types, cutoff_radius=cutoff_radius[i])
-            parameters_list.append(parameters__obj)
-        parameters = np.array(parameters_list)
-        return parameters
+    #Parameter class variables creation
+    def get_parameters(parameters, types, positions, box, temprature, update_radius,cutoff_radius, es_sigma, k_cutoff):
+        #Load input arrays from .npz format
+        parameters_fh, types, positions, box = Toolbox.get_inputs(parameters, types, positions, box)
+        #parameters object creation
+        parameters_1 =parameters
+        temprature_1 = temprature
+        update_radius_1 = update_radius
+        es_sigma_1 = es_sigma
+        K_cutoff_1 = k_cutoff
+        cutoff_radius_1 = cutoff_radius
+        #get particle_types 
+        particle_types, name, mass, charge, lj_epsilon, lj_sigma = Toolbox.get_particle_type(parameters_1, types, positions, box)
+        parameters_obj = Parameters(temprature_1, box, es_sigma_1,
+                                    update_radius_1, particle_types, cutoff_radius_1, K_cutoff_1)
+        return parameters_obj
     
     if __name__ == '__main__':
         print('Toolbox running')
+
+    
+    
--- Particle_Simulation/test/test_Toolbox.py
+++ Particle_Simulation/test/test_Toolbox.py
@@ -1,39 +1,102 @@
-import os
 import numpy as np
 import unittest
-from Particle_Simulation.LennardJones import LennardJones
+import numpy.testing as npt
 from Particle_Simulation.Toolbox import Toolbox
 
 
 class test_ToolBox(unittest.TestCase):
-    
-    #If InputFile in value of os.path.abspath or BuildDirectory
+    # parameter fetch test    
+    def test_print_inputs(self):
+        parameters = {'A' : [3.32, 0.011589, 22.98, 1.6],
+                      'B' : [4.40104, 0.4184, 35.453, 2.3]}
+        types =  ['A', 'B', 'B', 'A']
+        positions = np.asarray([[1.5, 1., 2.5],
+                                [1.5, 4., 2.5],
+                                [1.5, 4., 3.],
+                                [1.5, 4., 4.]])
+        box = np.asarray([20.0, 20.0, 20.0])
+        temprature = 120
+        update_radius = 2.
+        es_sigma = 3.5
+        k_cutoff = 4.
+        parameters, types, positions, box = Toolbox.get_inputs(parameters, types, positions, box)
+        ref_parameters = {'A': [3.32, 0.011589, 22.98, 1.6], 'B': [4.40104, 0.4184, 35.453, 2.3]}
+        ref_types = np.array(types)
+        ref_positions = np.array(positions)
+        ref_box = np.asarray(box)
+        self.assertEqual(ref_parameters, parameters, msg='Failed: Actual and Desired are not equal')
+        npt.assert_equal(ref_types, types, 'Failed', verbose=True)
+        npt.assert_equal(ref_positions, positions, 'Failed', verbose=True)
+        npt.assert_equal(ref_box, box, 'Failed', verbose=True)
+
     def test_get_particle(self):
-        reference_distance = 3.5
-        particle = Toolbox.get_particle(os.path.abspath("Input_Particle.csv"))
-        distance = LennardJones._calculate_distance(particle[0], particle[1])
-        self.assertEqual(reference_distance, distance, msg='Failed: Actual and Desired are not equal')
+        parameters = {'A' : [3.32, 0.011589, 22.98, 1.6],
+                      'B' : [4.40104, 0.4184, 35.453, 2.3]}
+        types =  ['A', 'B', 'B', 'A']
+        positions = np.asarray([[1.5, 1., 2.5],
+                                [1.5, 4., 2.5],
+                                [1.5, 4., 3.],
+                                [1.5, 4., 4.]])
+        box = np.asarray([20.0, 20.0, 20.0])
+        temprature = 120
+        update_radius = 2.
+        es_sigma = 3.5
+        k_cutoff = 4.
+        type_index_cal, position_cal, particle = Toolbox.get_particle(parameters, types, positions, box)
+        ref_positions = np.array(positions)
+        ref_type_index = np.array([0, 1, 1, 0])
+        ref_position = np.array(positions)
+        npt.assert_equal(ref_type_index, type_index_cal, 'Failed', verbose=True)
+        npt.assert_equal(ref_position, position_cal, 'Failed', verbose=True)
+    
+    def test_get_particle_type(self):
+        parameters = {'A' : [3.32, 0.011589, 22.98, 1.6],
+                      'B' : [4.40104, 0.4184, 35.453, 2.3]}
+        types =  ['A', 'B', 'B', 'A']
+        positions = np.asarray([[1.5, 1., 2.5],
+                                [1.5, 4., 2.5],
+                                [1.5, 4., 3.],
+                                [1.5, 4., 4.]])
+        box = np.asarray([20.0, 20.0, 20.0])
+        temprature = 120
+        update_radius = 2.
+        es_sigma = 3.5
+        k_cutoff = 4.
+        particle_type, name, mass, charge, lj_epsilon, lj_sigma  = Toolbox.get_particle_type(parameters, 
+                                                                                             types, positions, box)
+        ref_name = np.array(['A', 'B'])
+        ref_lj_sigma = np.array([3.32, 4.40104 ])
+        ref_lj_epsilon = np.array([0.011589, 0.4184 ])
+        ref_mass = np.array([22.98, 35.453 ])
+        ref_charge = np.array([1.6, 2.3 ])
+        npt.assert_equal(ref_name, name, 'Failed', verbose=True)
+        npt.assert_equal(ref_lj_sigma, lj_sigma, 'Failed', verbose=True)
+        npt.assert_equal(ref_lj_epsilon, lj_epsilon, 'Failed', verbose=True)
+        npt.assert_equal(ref_mass, mass, 'Failed', verbose=True)
+        npt.assert_equal(ref_charge, charge, 'Failed', verbose=True)
+        #print(particle_type[0].mass)
         
     def test_get_parameters(self):
-        reference_potential = -0.000042499
-        particle = Toolbox.get_particle(os.path.abspath("Input_Particle.csv"))
-        parameters = Toolbox.get_parameters("Input_Parameter.csv", "Input_ParticleType.csv")
-        lg_sigma = LennardJones._determine_sigma(particle[0], particle[1], parameters[0])
-        lg_value = LennardJones._calculate_potential(particle[0], particle[1], parameters[0])
-        lg_value_rounded = lg_value.round(decimals=9)
-        self.assertEqual(reference_potential, lg_value_rounded, msg='Failed: Actual and Desired are not equal')
-         
-    #If user inputs File path: 
-    #Build failing bcoz of no user input so commented
-    #However running 
-#     def test_user_input_get_particle(self):
-#         reference_distance = 3.5
-#         particle_file_path = input("Enter the Particle File Path: ")
-#         particle = Toolbox.get_particle(particle_file_path)
-#         distance = LennardJones._calculate_distance(particle[0], particle[1])
-#         self.assertEqual(reference_distance, distance, msg='Failed: Actual and Desired are not equal')
-             
+        parameters = {'A' : [3.32, 0.011589, 22.98, 1.6],
+                      'B' : [4.40104, 0.4184, 35.453, 2.3]}
+        types =  ['A', 'B', 'B', 'A']
+        positions = np.asarray([[1.5, 1., 2.5],
+                                [1.5, 4., 2.5],
+                                [1.5, 4., 3.],
+                                [1.5, 4., 4.]])
+        box = np.asarray([20.0, 20.0, 20.0])
+        temprature = 120
+        update_radius = .5
+        cutoff_radius = 3.
+        es_sigma = 3.5
+        k_cutoff = 2
+        parameters_obj = Toolbox.get_parameters(parameters, types, positions, box, 
+                                                temprature, update_radius,cutoff_radius, es_sigma, k_cutoff)
+        ref_temperature = 120
+        ref_box = np.asarray(box)
+        npt.assert_equal(ref_box, parameters_obj.box, 'Failed', verbose=True)
+        npt.assert_equal(ref_temperature,parameters_obj.temperature, 'Failed', verbose=True)
+        
     if __name__ == '__main__':
         unittest.main()
-        
-        
+    
